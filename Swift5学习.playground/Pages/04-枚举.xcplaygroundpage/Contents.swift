10a4b5f170bddb159b610a3f0585b30343cfafa1
CoderLeonidas <1538392301@qq.com>
2019-09-02 14:57:35 +0800

    提交主题目录

--- Swift5学习.playground/Pages/04-枚举.xcplaygroundpage/Contents.swift
+++ Swift5学习.playground/Pages/04-枚举.xcplaygroundpage/Contents.swift
@@ -23,7 +23,7 @@ dir = .north
 print(dir )
 
 switch dir {
-    case .north:                                                                                                                                                                                                              
+    case .north:
         print("north")
     case .south:
         print("south")
@@ -32,5 +32,196 @@ switch dir {
     case .west:
         print("west")
 }
+
+/*
+ 关联值
+ 有时将枚举的成员值和其他类型的值关联存储在一起回非常有用
+ */
+
+enum Score {
+    case points(Int)
+    case grade (Character)
+}
+// 每一个枚举值可以有更多维度
+var score = Score.points(96)
+score = .grade("A")
+
+switch score {
+    case let .points(i):
+        print(i, "points")
+    
+    case let .grade(i):
+        print(i, "grade")
+}
+
+enum Date {
+    case digit(year: Int, month: Int, day:Int)
+    case string(String)
+}
+
+// 必要时，let也可以改为var
+var date = Date.digit(year: 2011, month: 9, day: 10)
+date = .string("2011-09-10")
+switch date {
+    case .digit(let year, let month, let day):
+        print(year, month, day)
+    case let .string(value):
+        print(value)
+}
+
+/*
+ 关联值l举例
+ */
+
+enum Password {
+    case number(Int, Int, Int, Int)
+    case gesture(String)
+}
+
+var pwd = Password.number(3, 5, 7, 8)
+pwd = .gesture("12369")
+switch pwd {
+case let .number(n1, n2, n3, n4):
+    print("number is ", n1, n2, n3, n4)
+case let .gesture(str):
+    print("gesture is ", str)
+}
+
+
+/*
+ 枚举成员可以使用相同类型的默认值预先对应，这个默认值叫做：原始值
+ 
+ 注意：原始值不占用枚举变量的内存
+ */
+
+enum PokerSuit: Character {
+    case spade = "♠️"
+    case heart = "♥️"
+    case diamond = "♦️"
+    case club = "♣️"
+}
+
+var suit = PokerSuit.spade
+print(suit)
+print(suit.rawValue)
+print(PokerSuit.club.rawValue)
+
+enum Grade: String {
+    case perfect = "A"
+    case great = "B"
+    case good = "C"
+    case bad  = "D"
+}
+
+print(Grade.perfect.rawValue)
+print(Grade.great.rawValue)
+print(Grade.good.rawValue)
+print(Grade.bad.rawValue)
+
+/*
+ 隐式原始值
+ 如果枚举的原始值类型是Int、String，Swift会自动分配原始值
+ */
+
+enum Direction3: String {
+    case north = "north"
+    case south  = "south"
+    case east = "east"
+    case west = "west"
+}
+
+// 等价于
+
+enum Direction4: String {
+    case north, south, east , west
+}
+
+print(Direction3.north)
+print(Direction4.north.rawValue)
+
+enum Season: Int  {
+    case spring, summer, autumn, wiinter
+}
+print(Season.spring.rawValue)
+print(Season.summer.rawValue)
+print(Season.autumn.rawValue)
+print(Season.wiinter.rawValue)
+
+
+enum Season2: Int {
+    case spring = 1, summer, autumn = 4, winter
+}
+print(Season2.spring.rawValue)
+print(Season2.summer.rawValue)
+print(Season2.autumn.rawValue)
+print(Season2.winter.rawValue)
+
+/*
+ 递归枚举  ？？？？编译不通过
+ */
+
+//indirect enum ArithExpr {
+//    case number(Int)
+//    case sum(ArithExpr, ArithExpr)
+//    case difference(ArithExpr, ArithExpr)
+//}
+//
+//enum ArithExpr {
+//    case number(Int)
+//    indirect case sum(ArithExpr, ArithExpr)
+//    indirect case difference(ArithExpr, ArithExpr)
+//}
+//
+//let five = ArithExpr.number(5)
+//let four = ArithExpr.number(4)
+//let two  = ArithExpr.number(2)
+//let sum = ArithExpr.sum(five, four)
+//let difference = ArithExpr.difference(sum, two)
+//
+//func calculate(_ expr: ArithExpr) -> Int {
+//    switch expr {
+//    case let .number(value):
+//        return value
+//    case let .sum(left, right):
+//        return calculate(left) + calculate(right)
+//    case let .difference(left, right):
+//        return calculate(left) - calculate(right)
+//    }
+//}
+//
+//calculate(difference)
+
+/*
+ MemoryLayout
+ 可以用MemoryLayout 获取数据类型占用的内存大小
+ */
+
+enum Password1 {
+    case number(Int, Int, Int, Int)
+    case other
+}
+
+MemoryLayout<Password1>.stride // 40 分配占用的空间大小
+MemoryLayout<Password1>.size // 33 实际用到的空间大小
+MemoryLayout<Password1>.alignment // 8 对齐参数
+
+var pwd1 = Password1.number(9, 8, 6, 4)
+pwd1 = Password1.other
+MemoryLayout.stride(ofValue: pwd1) // 40
+MemoryLayout.size(ofValue: pwd1) // 33
+MemoryLayout.alignment(ofValue: pwd1) // 8
 //: [Next](@next)
+
+//: [Next](@next)
+
+//: [Next](@next)
+
+//: [Next](@next)
+
+//: [Next](@next)
+
+//: [Next](@next)
+
+//: [Next](@next)
+
 
--- Swift5学习.playground/Pages/05-可选项.xcplaygroundpage/Contents.swift
+++ Swift5学习.playground/Pages/05-可选项.xcplaygroundpage/Contents.swift
@@ -1,7 +1,130 @@
-//: [Previous](@previous)
 
 import Foundation
 
-var str = "Hello, playground"
+/*
+ 可选项，一般也叫做可选类型，它允许将值设置为nil
+ 在类型名称后面加个问号’?‘ 来定义一个可选项
+ */
+
+var name: String? = "Jack"
+name = nil // 如果没有？，则报错"'nil' cannot be assigned to type 'String'"
+
+
+var age: Int? // 默认值就是nil
+age = 10
+age = nil
+
+var array = [1,15,40,29]
+func get(_ index: Int)-> Int?  {
+    if (index < 0 || index >= array.count) {
+        return nil
+    }
+
+    return array[index]
+}
+print(get(1))
+print(get(-1))
+print(get(4))
+
+/*
+ 强制解包 forced unwrapping
+ 可选项是对其他类型的一层封装，也可以将它理解为一个盒子
+ 如果为nil，那么它是个空盒子
+ 如果不为nil，那么盒子里装的是：被包装类型的数据
+ 
+ var age: Int? // 默认值就是nil
+ age = 10
+ age = nil
+ 
+ 
+ */
+
+
+// 如果要从可选项中取出被包装的数据(将盒子里的东西取出开),需要使用感叹号'!'，进行强制解包
+
+var age1: Int? = 10
+var ageInt: Int = age1!
+ageInt += 10
+
+// 如果对值为nil 的可选项(空盒子)进行强制解包，将会产生运行时错误  ：Fatal error: Unexpectedly found nil while unwrapping an Optional value
+//var age2: Int ?
+//age!
+
+// 判断可选项是否包含值
+
+let number = Int("123")
+if number != nil {
+    print("字符串转换整数成功：\(number!)")
+    // number 是强制解包之后的Int值
+    // number的作用域仅限于这个大括号
+} else {
+    print("字符串转换整数失败")
+}
+
+/*
+  可选项值绑定
+ 
+ 可以使用可选项绑定来判断可选项是否包含值
+ 如果包含就自动解包，把值赋值给一个临时的常量let或者变量var，并返回true，否则返回false
+ */
+
+enum Season: Int {
+    case spring = 1, summer, autumn, winter
+}
+
+if let season = Season(rawValue: 6) {
+    switch season {
+    case .spring:
+        print("the season is spring")
+    default:
+        print("the season is other")
+    }
+} else {
+    print("no such season")
+}
+
+/*
+ 以下两种写法是等价的
+ */
+
+if let first = Int("4") {
+    if let second = Int("42") {
+        if first < second && second < 100 {
+            print("\(first) < \(second) < 100")
+        }
+    }
+}
+
+if let first = Int("4"), let second = Int("42"), first < second && second < 100 {
+    print("\(first) < \(second) < 100")
+
+}
+
+/*
+ while循环中使用可选项值绑定
+
+ */
+// 遍历数组，将遇到的整数都加起来，如果遇到负数或者非数字，则停止遍历
+var strs = ["10", "20", "abc", "-20", "30"]
+var index = 0
+var sum = 0
+while let num = Int(strs[index]), num > 0 {
+    sum += num
+    index += 1
+}
+print(sum)
+
+/*
+ 空合并运算符 ?? （nil - coalescing operator）
+1 a ?? b
+ 11 a是可选项
+ 12 b是可选项 或者 不是可选项
+ 13 b和a的存储类型必须相同
+
+2 如果a 不为nil，就返回a
+3 如果a 为nil，就返回b
+4 如果b不是可选项，返回a时会自动解包
+ */
+
 
 //: [Next](@next)
